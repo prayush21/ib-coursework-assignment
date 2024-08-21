389d5500503cff792916ac10232b4af093a5b681
prayush21 <prayushdave@gmail.com>
2024-08-21 19:47:01 +0530

    Add Button on Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
-  return <main className="">Hello</main>;
+  return (
+    <main className="">
+      <Button variant="default">
+        <Link href="/coursework">Go to Coursework Page</Link>{" "}
+      </Button>
+    </main>
+  );
 }
